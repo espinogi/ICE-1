0f56761f1d3ca7b6d3735796ed177a4f4f461ab5
espinogi <espinogi@sheridancollege.ca>
2024-09-17 23:33:18 -0400

    ICE 1

---
+++ Giancarlo_Espinosa_ICE1/src/giancarlo_espinosa_ice1/Giancarlo_Espinosa_ICE1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package giancarlo_espinosa_ice1;
+
+/**
+ *
+ * @author giancarloespinosa
+ */
+public class Giancarlo_Espinosa_ICE1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
