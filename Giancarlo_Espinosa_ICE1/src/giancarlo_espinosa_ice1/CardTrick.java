ea0f341efba32a08384f89f963b5934d56b30ae4
espinogi <espinogi@sheridancollege.ca>
2024-09-18 01:03:24 -0400

    Update CardTrick.java
    added comments

--- Giancarlo_Espinosa_ICE1/src/giancarlo_espinosa_ice1/CardTrick.java
+++ Giancarlo_Espinosa_ICE1/src/giancarlo_espinosa_ice1/CardTrick.java
@@ -17,7 +17,8 @@ public class CardTrick {
     public static void main(String[] args) {
         // Create an instance of Random
         Random random = new Random();
-        
+
+        // Creating the magic hand
         Card[] magicHand = new Card[7];
         
         for (int i = 0; i < magicHand.length; i++) {
@@ -26,21 +27,24 @@ public class CardTrick {
             c.setSuit(Card.SUITS[random.nextInt(4)]); // Generates a random suit from the SUITS array
             magicHand[i] = c;
         }
-
+        // Getting the user input
         Scanner scan = new Scanner(System.in);
-        System.out.println("Pick a card value (1-13): ");
-        int userValue = scan.nextInt();
-        System.out.println("Pick a card suit (Diamonds, Spades, Clubs, Hearts): ");
-        String userSuit = scan.next();
-
+            System.out.println("Pick a card value (1-13): ");
+                int userValue = scan.nextInt();
+            System.out.println("Pick a card suit (Diamonds, Spades, Clubs, Hearts): ");
+                String userSuit = scan.next();
+        
+        // Creating the lucky card's value and suit
         Card luckyCard = new Card();
-        luckyCard.setValue(4);
-        luckdyCard.setSuit("Diamonds");
-        
+            luckyCard.setValue(4);
+            luckdyCard.setSuit("Diamonds");
+
+        // Getting the users card value and suit
         Card userCard = new Card();
-        userCard.setValue(userValue);
-        userCard.setSuit(userSuit);
-        
+            userCard.setValue(userValue);
+            userCard.setSuit(userSuit);
+
+        // Checking if the lucky card and user card match
         boolean found = false;
         for (Card card : magicHand) {
             if(card.equals(luckyCard)) {
@@ -48,10 +52,12 @@ public class CardTrick {
                 break;
             }
         }
-        
+
+        // Message if the user's card matches the lucky card
         if(found) {
             System.out.println("The lucky card was in the magic hand! You win!");
-            
+
+        // Message if the user's card doesn't match the lucky card
         } else {
             System.out.println("The lucky card was not in the magic hand! You lose.");
         }
